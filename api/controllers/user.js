a4ca6f760d0921944ad14380bfa94a6e318b211e
Christian Zarur <christianzarurm@gmail.com>
2018-06-03 13:12:14 -0400

    registro y usuarios duplicados

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,5 +1,5 @@
 'use strict'
-
+var bcrypt = require('bcrypt-nodejs');
 var User = require('../models/user');
 
 function home(req, res) {
@@ -14,7 +14,54 @@ function pruebas(req, res) {
     });
 }
 
+function saveUser(req, res) {
+    var params = req.body;
+    var user = new User();
+
+    if (params.nombre && params.apellido && params.nick && params.password){
+        user.nombre = params.nombre;
+        user.apellido = params.apellido;
+        user.nick = params.nick;
+        user.email = params.email;
+        user.rol = 'ROL_USER';
+        user.imagen = null;
+
+        //CONTROLAR USUARIOS DUPLICADOS
+        User.find({ $or: [
+            {email: user.email.toLowerCase()},
+            {nick: user.nick.toLowerCase()}
+        ]}).exec((err, users)=> {
+            if (err) return res.status(500).send({message: 'Error en la peticion de usuarios'});
+
+            if (users && users.length >=1){
+                return res.status(200).send({message: 'El usuario ya existe'});
+            }
+            else{
+                //CIFRA CONTRASEÑA Y GUARDA LOS DATOS
+                bcrypt.hash(params.password, null, null, (err, hash)=>{
+                    user.password = hash;
+                    user.save((err,userStored)=>{
+                        if(err) return res.status(500).send({message: 'Error al guardar el usuario'});
+
+                        if(userStored){
+                            res.status(200).send({user: userStored});
+                        }
+                        else{
+                            res.status(404).send({message: 'No se ha registrado el usuario'});
+                        }
+                    });
+                });
+            }
+        });
+    }
+    else{
+                res.status(200).send({
+                message: 'Envia todos los campos necesarios'
+                })
+            }
+}
 module.exports = {
     home,
-    pruebas
+    pruebas,
+    saveUser
 }
