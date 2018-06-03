2c2ae28db125731e80664e9e82706d41483f9cf9
Christian Zarur <christianzarurm@gmail.com>
2018-06-02 23:52:50 -0400

    servidor web

--- api/index.js
+++ api/index.js
@@ -1,10 +1,18 @@
 'use strict'
 
 var mongoose = require('mongoose');
+var app = require('./app');
+var port = 3800;
 
+//conexion db
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/mean_social', {useMongoClient: true})
         .then(()=>{
             console.log("La conexion a la base de datos fue exitosa");
+
+            //crear servidor
+            app.listen(port, () =>{
+                console.log("servidor corriendo en http://localhost:3800");
+            })
         })
         .catch(err => console.log(err));
