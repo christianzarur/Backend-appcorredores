40d294238bb9c0d32ed30c714f1b6cc2544d8dd5
Christian Zarur <christianzarurm@gmail.com>
2018-06-07 23:41:11 -0400

    timeline

--- api/controllers/publication.js
+++ api/controllers/publication.js
@@ -39,7 +39,40 @@ function savePublication(req, res) {
     });
 }
 
+function getPublications(req, res) {
+    var page = 1;
+    if (req.params.page) {
+        page = req.params.page;
+    }
+
+    var itemsPerPage = 4;
+
+    Follow.find({user: req.user.sub}).populate('seguido').exec((err, follows) => {
+        if (err) res.status(500).send({message: "Error al devolver el seguimiento"});
+
+        var follows_clean =[];
+
+        follows.forEach((follow) => {
+            follows_clean.push(follow.seguido);
+        });
+        Publication.find({user: {"$in": follows_clean}}).sort('-fecha').populate('user').paginate(page, itemsPerPage, (err, publications, total) =>{
+            if (err) res.status(500).send({message: "Error al devolver publicaciones"});
+
+            if (!publications) res.status(404).send({message: "No hay publicaciones"});
+
+            return res.status(200).send({
+                total_items: total,
+                pages: Math.ceil(total/itemsPerPage),
+                page: page,
+                publications
+            })
+        });
+    });
+
+}
+
 module.exports = {
     probando,
-    savePublication
+    savePublication,
+    getPublications
 }
--- api/routes/publication.js
+++ api/routes/publication.js
@@ -9,6 +9,7 @@ var multipart = require('connect-multiparty');
 var md_upload = multipart({uploadDir: './uploads/publications'});
 
 api.get('/probando-pub', md_auth.ensureAuth, PublicactionController.probando);
+api.get('/publications/:page?', md_auth.ensureAuth, PublicactionController.getPublications);
 
 api.post('/publication', md_auth.ensureAuth, PublicactionController.savePublication);
 
