c7a8d1384ac4a9be005388ac08479becd77de165
Christian Zarur <christianzarurm@gmail.com>
2018-06-10 02:35:59 -0400

    componentes

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
+//Componentes
 import { AppComponent } from './app.component';
-import { LoginComponent } from './app/components/login/login/login.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule
