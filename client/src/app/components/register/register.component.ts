c547506d3c2430f3bc82c3390b3e91a1812c7da1
Christian Zarur <christianzarurm@gmail.com>
2018-06-10 20:58:02 -0400

    servicio usuarios

--- client/src/app/components/register/register.component.ts
+++ client/src/app/components/register/register.component.ts
@@ -1,17 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { User } from '../models/user';
+import { UserService } from "../../services/user.service";
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
+  providers: [UserService]
 })
 export class RegisterComponent implements OnInit {
   public title: string;
   public user: User;
   constructor(
     private _route: ActivatedRoute,
-    private _router: Router
+    private _router: Router,
+    private _userService: UserService
   ) {
     this.title = 'Regístrate'
     this.user = new User("",
@@ -29,6 +32,6 @@ export class RegisterComponent implements OnInit {
   }
 
   onSubmit(){
-    console.log(this.user);
+    this._userService.register(this.user);
   }
 }
