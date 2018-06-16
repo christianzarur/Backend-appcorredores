5f9555777a2038063a40d5d74af9ca9ca02ac512
Christian Zarur <christianzarurm@gmail.com>
2018-06-15 21:51:42 -0400

    componentes users

--- client/src/app/app.routing.ts
+++ client/src/app/app.routing.ts
@@ -6,6 +6,7 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { HomeComponent } from './components/home/home.component';
 import { UserEditComponent } from './components/user-edit/user-edit.component';
+import { UsersComponent } from "./components/users/users.component";
 
 
 const appRoutes: Routes =[
@@ -14,8 +15,9 @@ const appRoutes: Routes =[
     {path: 'login', component: LoginComponent},
     {path: 'registro', component: RegisterComponent},
     {path: 'mis-datos', component: UserEditComponent},
+    {path: 'gente', component: UsersComponent },
 
-    //{path: '**', pathMatch: 'full', redirectTo: 'login'}
+    {path: '**', pathMatch: 'full', redirectTo: 'home'}
 ];
 
 export const appRoutingProviders: any[] = [];
