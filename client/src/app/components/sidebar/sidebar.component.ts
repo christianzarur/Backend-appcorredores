4810729e6a254a57ed60a55857399bd461443126
Christian Zarur <christianzarurm@gmail.com>
2018-06-17 12:10:12 -0400

    servicios pub

--- client/src/app/components/sidebar/sidebar.component.ts
+++ client/src/app/components/sidebar/sidebar.component.ts
@@ -2,12 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../services/user.service';
 import { GLOBAL } from '../../services/global';
 import { Publication } from "../models/publication";
+import { PublicationService } from '../../services/publication.service';
+
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css'],
-  providers: [UserService]
+  providers: [UserService, PublicationService]
 })
 export class SidebarComponent implements OnInit {
   public url;
@@ -18,7 +20,8 @@ export class SidebarComponent implements OnInit {
   public publication: Publication;
 
   constructor(
-    private _userService: UserService
+    private _userService: UserService,
+    private _publicationService: PublicationService
   ) { 
     this.identity = this._userService.getIdentity();
     this.token = this._userService.getToken();
@@ -31,7 +34,25 @@ export class SidebarComponent implements OnInit {
     console.log("sidebar cargado")
   }
 
-  onSubmit(){
-    console.log(this.publication)
+  onSubmit(form){
+
+    this._publicationService.addPublication(this.token, this.publication).subscribe(
+      response => {
+        if (response.publication) {
+          //this.publication = response.publication;
+          this.status = 'exitoso';
+          form.reset();
+        } else {
+          this.status = 'error';
+        }
+      },
+      error => {
+        var errorMsj = <any>error;
+        console.log(errorMsj);
+        if (errorMsj != null) {
+          this.status = 'error'
+        }
+      }
+    );
   }
 }
---
+++ client/src/app/services/publication.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { GLOBAL } from './global';
+import { Publication } from '../components/models/publication';
+
+@Injectable()
+
+export class PublicationService {
+    public url: string;
+
+
+    constructor(public _http: HttpClient) {
+        this.url = GLOBAL.url;
+    }
+
+    addPublication(token, publication): Observable<any> {
+        let params = JSON.stringify(publication);
+        let headers = new HttpHeaders().set('Content-type', 'application/json')
+                                        .set('Authorization', token);
+        return this._http.post(this.url + 'publication', params, { headers: headers});
+    }
+
+    getPublicatons(token, page = 1): Observable<any>{
+        let headers = new HttpHeaders().set('Content-type', 'application/json')
+                                        .set('Authorization', token); 
+        return this._http.get(this.url + 'publications/'+page, { headers: headers });
+                
+    }
+
+    deletePublication(token, id): Observable<any>{
+        let headers = new HttpHeaders().set('Content-type', 'application/json')
+                                        .set('Authorization', token);
+        return this._http.delete(this.url + 'publications/' + id, { headers: headers });
+    }
+}
