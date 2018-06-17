22f68180e83f5c5ce4cc8e1a35e6e73f04502570
Christian Zarur <christianzarurm@gmail.com>
2018-06-17 12:48:28 -0400

    ver pubs

--- client/src/app/components/timeline/timeline.component.ts
+++ client/src/app/components/timeline/timeline.component.ts
@@ -3,12 +3,14 @@ import { Router, ActivatedRoute, Params } from '@angular/router';
 import { Publication } from '../models/publication';
 import { GLOBAL } from '../../services/global';
 import { UserService } from '../../services/user.service';
+import { PublicationService } from '../../services/publication.service';
+
 
 @Component({
   selector: 'app-timeline',
   templateUrl: './timeline.component.html',
   styleUrls: ['./timeline.component.css'],
-  providers: [UserService]
+  providers: [UserService, PublicationService]
 
 })
 export class TimelineComponent implements OnInit {
@@ -16,21 +18,56 @@ export class TimelineComponent implements OnInit {
   public identity;
   public token;
   public url: string;
+  public status:string;
+  public page;
+  public publications: Publication[];
+  public total;
+  public pages;
 
   constructor(private _route: ActivatedRoute,
   private _router: Router,
-  private _userService: UserService
+  private _userService: UserService,
+  private _publicationService: PublicationService
   ) {
     this.title = 'Timeline';
     this.identity = this._userService.getIdentity();
     this.token = this._userService.getToken();
     this.url = GLOBAL.url;
+    this.page =1;
 
    }
   
 
   ngOnInit() {
     console.log('Timeline cargado');
+    this.getPublications(this.page);
   }
 
+  getPublications(page){
+    this._publicationService.getPublicatons(this.token, page).subscribe(
+      response=>{
+        if (response.publications) {
+          this.total = response.total_items;
+          this.publications = response.publications;
+          this.pages= response.pages;
+
+          if (page>this.pages) {
+            this._router.navigate(['/home']);
+          }
+
+
+        }else{
+          this.status='error';
+        }
+
+      },
+      error =>{
+        var errorMsj = <any>error;
+        console.log(errorMsj);
+        if (errorMsj != null) {
+          this.status = 'error'
+        }
+      }
+    )
+  }
 }
